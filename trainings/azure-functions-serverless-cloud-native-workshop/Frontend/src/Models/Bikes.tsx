6e3a8a69b82461b7a68a4014d7064edf1689d355
Dan Biscup <dabiscup@microsoft.com>
2022-03-21 10:37:09 -0600

    azf

---
+++ trainings/azure-functions-serverless-cloud-native-workshop/Frontend/src/Models/Bikes.tsx
@@ -0,0 +1,18 @@
+class Bike {
+    id: string;
+    model: string;
+    make: string;
+    price: number;
+    quantity: number;
+   
+    constructor() 
+    {
+        this.id = "";
+        this.model = "";
+        this.make = "";
+        this.price = 0;
+        this.quantity = 0;
+    }
+  }
+
+  export default Bike

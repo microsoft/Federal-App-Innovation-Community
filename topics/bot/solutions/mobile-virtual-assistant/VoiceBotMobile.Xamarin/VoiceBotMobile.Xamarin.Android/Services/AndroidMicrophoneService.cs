7a6e516ccd591b14753a2a06e58f1b1a77ac6116
nkidambi <78823488+nkidambi@users.noreply.github.com>
2022-05-20 16:29:39 -0400

    initial commit

---
+++ topics/bot/solutions/mobile-virtual-assistant/VoiceBotMobile.Xamarin/VoiceBotMobile.Xamarin.Android/Services/AndroidMicrophoneService.cs
@@ -0,0 +1,75 @@
+﻿using Android;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.Core.App;
+using Google.Android.Material.Snackbar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoiceBotMobile.Xamarin.Droid.Services;
+using VoiceBotMobile.Xamarin.Services;
+using Xamarin.Forms;
+
+namespace VoiceBotMobile.Xamarin.Droid.Services
+{
+    internal class AndroidMicrophoneService : IMicrophoneService
+    {
+        public const int REQUEST_MIC = 1;
+        private TaskCompletionSource<bool> tcsPermissions;
+        private readonly string[] permissions = new string[] { Manifest.Permission.RecordAudio, Manifest.Permission.ReadExternalStorage };
+
+        public Task<bool> GetPermissionAsync()
+        {
+            tcsPermissions = new TaskCompletionSource<bool>();
+
+            if ((int)Build.VERSION.SdkInt < 23)
+            {
+                tcsPermissions.TrySetResult(true);
+            }
+            else
+            {
+                var currentActivity = MainActivity.Instance;
+                RequestMicPermission();
+                if ((ActivityCompat.CheckSelfPermission(currentActivity, Manifest.Permission.RecordAudio) != (int)Android.Content.PM.Permission.Granted) ||
+                    ActivityCompat.CheckSelfPermission(currentActivity, Manifest.Permission.ReadExternalStorage) != (int)Android.Content.PM.Permission.Granted)
+                {
+                    RequestMicPermission();
+                }
+                else
+                {
+                    tcsPermissions.TrySetResult(true);
+                }
+            }
+
+            return tcsPermissions.Task;
+        }
+
+        private void RequestMicPermission()
+        {
+            var currentActivity = MainActivity.Instance;
+            if (ActivityCompat.ShouldShowRequestPermissionRationale(currentActivity, Manifest.Permission.RecordAudio))
+            {
+                Snackbar.Make(currentActivity.FindViewById((Android.Resource.Id.Content)), "App requires microphone permission.", Snackbar.LengthIndefinite).SetAction("Ok", v =>
+                {
+                    ((Activity)currentActivity).RequestPermissions(permissions, REQUEST_MIC);
+                }).Show();
+            }
+            else
+            {
+                ActivityCompat.RequestPermissions(((Activity)currentActivity), permissions, REQUEST_MIC);
+            }
+        }
+
+        public void OnRequestPermissionResult(bool isGranted)
+        {
+            tcsPermissions.TrySetResult(isGranted);
+        }
+    }
+}
---
+++ topics/bot/solutions/mobile-virtual-assistant/VoiceBotMobile.Xamarin/VoiceBotMobile.Xamarin/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoiceBotMobile.Xamarin
+{
+    public static class Constants
+    {
+        // API key can be a shared, multi-resource key or an individual service key and can be found
+        // and regenerated in the Azure portal
+        public static string CognitiveServicesApiKey = "24838d27e2b749b9aaf27bd2c1ea506";
+
+        // Endpoint is based on your configured region, for example "westus"
+        public static string CognitiveServicesRegion = "westus2";
+    }
+}

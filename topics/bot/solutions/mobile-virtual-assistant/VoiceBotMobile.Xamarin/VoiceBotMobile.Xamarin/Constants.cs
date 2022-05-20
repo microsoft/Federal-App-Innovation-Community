1a8171a7c4774aa1c4593c88724e6cb08446d8bd
nkidambi <78823488+nkidambi@users.noreply.github.com>
2022-05-20 16:57:28 -0400

    Remove keys

--- topics/bot/solutions/mobile-virtual-assistant/VoiceBotMobile.Xamarin/VoiceBotMobile.Xamarin/Constants.cs
+++ topics/bot/solutions/mobile-virtual-assistant/VoiceBotMobile.Xamarin/VoiceBotMobile.Xamarin/Constants.cs
@@ -8,9 +8,9 @@ namespace VoiceBotMobile.Xamarin
     {
         // API key can be a shared, multi-resource key or an individual service key and can be found
         // and regenerated in the Azure portal
-        public static string CognitiveServicesApiKey = "24838d27e2b749b9aaf27bd2c1ea506";
+        public static string CognitiveServicesApiKey = "";
 
         // Endpoint is based on your configured region, for example "westus"
-        public static string CognitiveServicesRegion = "westus2";
+        public static string CognitiveServicesRegion = "";
     }
 }

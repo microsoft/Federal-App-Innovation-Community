39790085d4294b5511712d1e462c9d164c2e3d8d
Greg Roe <gregroe@msn.com>
2023-03-02 12:37:36 -0800

    portal configuration and test

--- topics/power-platform/solutions/powerpages-gbyte-fileupload-datalake/DeploymentScript/deployazuresolution.ps1
+++ topics/power-platform/solutions/powerpages-gbyte-fileupload-datalake/DeploymentScript/deployazuresolution.ps1
@@ -245,6 +245,109 @@ Write-Host "...Static Web App uploaded"
     
 
 
+
+
+#####################################################################################################################
+
+Write-Host "Entering Power Platform Deployment." -ForegroundColor Green
+Write-Host " "
+
+$Title = "Logging into Cloud"
+$Info = "Choose a Cloud" 
+$options = [System.Management.Automation.Host.ChoiceDescription[]] @("&1 Public", "&2 UsGov", "&3 UsGovHigh","&4 UsGovDod")
+[int]$defaultchoice = 1
+$opt = $host.UI.PromptForChoice($Title,$Info ,$Options,$defaultchoice)
+switch($opt)
+{
+0 { $Cloud = "Public"}
+1 { $Cloud = "UsGov"}
+2 { $Cloud = "UsGovHigh"}
+3 { $Cloud = "UsGovDod"}
+}
+Write-Host "Logging you into Cloud:" -ForegroundColor Green
+Write-Host $Cloud -ForegroundColor Green
+
+pac auth clear
+pac auth create --name "admin" --cloud $Cloud
+pac admin list
+
+$Title = "Create a new Dataverse Environment?"
+$Info = "Yes/No" 
+$options = [System.Management.Automation.Host.ChoiceDescription[]] @("&Yes", "&No")
+[int]$defaultchoice = 1
+$opt = $host.UI.PromptForChoice($Title,$Info ,$Options,$defaultchoice)
+switch($opt)
+{
+0 { Write-Host "Createing a new Environment"
+  $EnvironmentName = Read-Host "Enter an Environment Name: "
+  pac admin create --name $EnvironmentName --type Sandbox --region $Cloud
+}
+1 { $EnvironmentName = Read-Host "Which Existing Env are you deploying to? (cut/paste Environment name here) "}
+}
+
+Write-Host " Data Verse Environment $EnvironmentName is ready." -ForegroundColor Green
+
+Write-Host " "
+Write-Host " Portal Preparation..  THere are a couple manual steps. See instructions below. Press Enter when complete...... "   -ForegroundColor Green
+Write-Host " "
+Write-Host " Reference: https://learn.microsoft.com/en-us/power-apps/maker/portals/admin/migrate-portal-configuration?tabs=CLI#prepare-the-target-environment" -ForegroundColor DarkYellow
+Write-Host " "
+#CREATE CONNECTIONS IN THE TARGET ENVIRONMENT PRIOR TO IMPORTING SOLUTION
+Write-Host "STEP 1: In ""$EnvironmentName"" in the Power Apps Studio manually create a new Portal. Go to make.powwerapps select  Apps /New App /Website/Starter Portal" -ForegroundColor Green
+Write-Host "STEP 2: In the Portal Managment App, delete the newly created portal record" -ForegroundColor Green
+Write-Host "STEP 3: Delete the Portal app in powerapps studio (NOT the portal managment app)" -ForegroundColor Green
+Write-Host " "
+$continue = Read-Host "IMPORTANT!! Only When previous 3 portal provisioning steps are completed... then press Enter to continue....."
+
+Write-Host " "
+Write-Host "Importing Portal Configuration.... to $EnvironmentURL " -ForegroundColor Green
+Write-Host "Creating a Target profile. "
+pac auth create --environment $EnvironmentName  --Cloud $Cloud --Name "TargetEnvironment"
+pac auth select --name "TargetEnvironment"
+pac auth list
+Write-Host " "
+pac paportal upload --path .\portalconfig\starter-portal
+
+
+
+
+Write-Host ""
+Write-Host "MANUALLY Create the following connections in $EnvironmentName. When Complete, press the Enter Key....." -ForegroundColor Green
+Write-Host ""
+Write-Host "Microsoft Teams"  -ForegroundColor Green
+Write-Host "Approvals" -ForegroundColor Green
+Write-Host "Microsoft Dataverse" -ForegroundColor Green
+Write-Host "Office 365 Outlook" -ForegroundColor Green
+Write-Host "Office 365 Groups" -ForegroundColor Green
+Write-Host "Azure Event Grid" -ForegroundColor Green
+Write-Host ""
+$continue = Read-Host "IMPORTANT!!  Only When you've completed creating the above Connections ... Press Enter to continue....."
+
+Write-Host " "
+Write-Host " Connections created.  Now we'll import the Dataverse Solution...." -ForegroundColor Green
+pac solution import --path .\solution\PortalFileUpload.zip
+
+Write-Host " "
+Write-Host "Provision a new Portal WebSite with the imported configuration" -ForegroundColor Green
+Write-Host " "
+Write-Host "In ""$EnvironmentName"" Go to make.powwerapps select  Apps /New App /Website/Starter Portal" -ForegroundColor Green
+Write-Host "Select the Checkbox "" Use Data from existing website record. an Select the Starter portal that was imported" -ForegroundColor Green
+Write-Host "Reference https://learn.microsoft.com/en-us/power-pages/admin/migrate-site-configuration?tabs=CLI#prepare-the-target-environment" -ForegroundColor DarkYellow
+Write-Host " "
+$continue = Read-Host " Enter to continue....."
+
+
+
+
+
+
+
+
+
+
+
+
+
 Write-Host " "
 Write-Host " "
 Write-Host ".....End of Deployment Script"
--- topics/power-platform/solutions/powerpages-gbyte-fileupload-datalake/DeploymentScript/deploypowerplatform.ps1
+++ topics/power-platform/solutions/powerpages-gbyte-fileupload-datalake/DeploymentScript/deploypowerplatform.ps1
@@ -1,6 +1,6 @@
 
 
-Write-Host "Entering Power Platform Deployment." -ForegroundColor DarkCyan
+Write-Host "Entering Power Platform Deployment." -ForegroundColor Green
 Write-Host " "
 
 $Title = "Logging into Cloud"
@@ -15,15 +15,12 @@ switch($opt)
 2 { $Cloud = "UsGovHigh"}
 3 { $Cloud = "UsGovDod"}
 }
-Write-Host "Logging you into Cloud:"
+Write-Host "Logging you into Cloud:" -ForegroundColor Green
 Write-Host $Cloud -ForegroundColor Green
 
 pac auth clear
 pac auth create --name "admin" --cloud $Cloud
-
-
 pac admin list
-
 
 $Title = "Create a new Dataverse Environment?"
 $Info = "Yes/No" 
@@ -36,42 +33,73 @@ switch($opt)
   $EnvironmentName = Read-Host "Enter an Environment Name: "
   pac admin create --name $EnvironmentName --type Sandbox --region $Cloud
 }
-1 { $EnvironmentName = Read-Host "Which Env are you deploying to? "}
+1 { $EnvironmentName = Read-Host "Which Existing Env are you deploying to? (cut/paste Environment name here) "}
 }
 
 Write-Host " Data Verse Environment $EnvironmentName is ready." -ForegroundColor Green
 
-# PREPARE TARGET PORTAL ENVIRONMENT
-# Manual Portal Migration Steps as documented here   https://learn.microsoft.com/en-us/power-apps/maker/portals/admin/migrate-portal-configuration?tabs=CLI#prepare-the-target-environment
-#   1st Create a portal manually
-#   2nd  In the portal management app, delete that new portal record
-#   3rd in Power Apps Studio delete the portal app (NOT the portal management app)
-# When you are done, hit the enter key
-# 1 pac auth create  -ci "Public" --name "Greg Roe's Environment" --url https://orgec197a1b.crm.dynamics.com/
-# pac auth clear
-# 
-Write-Host " Portal Preparation..    There are a few manual steps as outlined in https://learn.microsoft.com/en-us/power-apps/maker/portals/admin/migrate-portal-configuration?tabs=CLI#prepare-the-target-environment" -ForegroundColor Green
-
+Write-Host " "
+Write-Host " Portal Preparation..  THere are a couple manual steps. See instructions below. Press Enter when complete...... "   -ForegroundColor Green
+Write-Host " "
+Write-Host " Reference: https://learn.microsoft.com/en-us/power-apps/maker/portals/admin/migrate-portal-configuration?tabs=CLI#prepare-the-target-environment" -ForegroundColor DarkYellow
+Write-Host " "
 #CREATE CONNECTIONS IN THE TARGET ENVIRONMENT PRIOR TO IMPORTING SOLUTION
-Write-Host "Step 1: In $EnvironmentName in the Power Apps Studio manually create a new Portal. Go to make.powwerapps select  Apps /New App /Website/Starter Portal"
-Write-Host "Step 2: In the Portal Managment App, delete the newly created portal record"
-Write-Host "Step 3: Delete the Portal app in powerapps studio (NOT the portal managment app)"
-$continue = Read-Host "WHEN PORTAL IS CREATED.. press any key to continue....."
+Write-Host "STEP 1: In ""$EnvironmentName"" in the Power Apps Studio manually create a new Portal. Go to make.powwerapps select  Apps /New App /Website/Starter Portal" -ForegroundColor Green
+Write-Host "STEP 2: In the Portal Managment App, delete the newly created portal record" -ForegroundColor Green
+Write-Host "STEP 3: Delete the Portal app in powerapps studio (NOT the portal managment app)" -ForegroundColor Green
+Write-Host " "
+$continue = Read-Host "IMPORTANT!! Only When previous 3 portal provisioning steps are completed... then press Enter to continue....."
 
-Write-Host "Importing Solution into $EnvironmentName"
-Write-Host "Step  1 First Manually Create the following connections:"
-Write-Host "Microsoft Teams"
-Write-Host "Approvals"
-Write-Host "Microsoft Dataverse"
-Write-Host "Office 365 Outlook"
-Write-Host "Office 365 Groups"
-Write-Host "Azure Event Grid"  ##  WHY IS THIS STILL HERE??? SHOULD BE GONE
-$continue = Read-Host "WHEN Connections are CREATED.. press any key to continue....."
+Write-Host " "
+Write-Host "Importing Portal Configuration.... to $EnvironmentURL " -ForegroundColor Green
+Write-Host "Creating a Target profile. "
+pac auth create --environment $EnvironmentName  --Cloud $Cloud --Name "TargetEnvironment"
+pac auth select --name "TargetEnvironment"
+pac auth list
+Write-Host " "
+pac paportal upload --path .\portalconfig\starter-portal
 
 
 
-#Upload Solution to the new or existing environment
-Write-Host "Step 2, automatically Deploying Solution to Environment: $EnvironmentName.  Please stand by..." -ForegroundColor Green
+
+Write-Host ""
+Write-Host "MANUALLY Create the following connections in $EnvironmentName. When Complete, press the Enter Key....." -ForegroundColor Green
+Write-Host ""
+Write-Host "Microsoft Teams"  -ForegroundColor Green
+Write-Host "Approvals" -ForegroundColor Green
+Write-Host "Microsoft Dataverse" -ForegroundColor Green
+Write-Host "Office 365 Outlook" -ForegroundColor Green
+Write-Host "Office 365 Groups" -ForegroundColor Green
+Write-Host "Azure Event Grid" -ForegroundColor Green
+Write-Host ""
+$continue = Read-Host "IMPORTANT!!  Only When you've completed creating the above Connections ... Press Enter to continue....."
+
+Write-Host " "
+Write-Host " Connections created.  Now we'll import the Dataverse Solution...." -ForegroundColor Green
+pac solution import --path .\solution\PortalFileUpload.zip
+
+Write-Host " "
+Write-Host "Provision a new Portal WebSite with the imported configuration" -ForegroundColor Green
+Write-Host " "
+Write-Host "In ""$EnvironmentName"" Go to make.powwerapps select  Apps /New App /Website/Starter Portal" -ForegroundColor Green
+Write-Host "Select the Checkbox "" Use Data from existing website record. an Select the Starter portal that was imported" -ForegroundColor Green
+Write-Host "Reference https://learn.microsoft.com/en-us/power-pages/admin/migrate-site-configuration?tabs=CLI#prepare-the-target-environment" -ForegroundColor DarkYellow
+Write-Host " "
+$continue = Read-Host " Enter to continue....."
+
+Write-Host " "
+Write-Host "All Done! go test your web site.  "
+
+
+
+
+
+
+
+
+
+
+
 
 
 
